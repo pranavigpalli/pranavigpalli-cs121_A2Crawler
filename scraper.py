b94b90bc38e598362719ee80436931b2503f8473
Audrey Lu <luay5@uci.edu>
2025-02-11 04:05:43 +0000

    Check for unwanted file extensions in query parameters

--- scraper.py
+++ scraper.py
@@ -130,16 +130,24 @@ def is_valid(url):
                 time.sleep(0.5 - elapsed)
         last_access[hostname] = time.time()
 
-        return re.match(r'^(.+\.)?(ics\.uci\.edu|cs\.uci\.edu|informatics\.uci\.edu|stat\.uci\.edu)$', parsed.netloc) and \
-            not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
+        # unwanted file extensions
+        pattern = r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
             + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$"
+        # check that queries do not have unwanted file extensions
+        queries = parse_qs(parsed.query)
+        for values in queries.values():
+            for value in values:
+                if re.match(pattern, value.lower()):
+                    return False
+
+        return re.match(r'^(.+\.)?(ics\.uci\.edu|cs\.uci\.edu|informatics\.uci\.edu|stat\.uci\.edu)$', parsed.netloc) and \
+            not re.match(pattern, parsed.path.lower())
 
     except TypeError:
         print ("TypeError for ", parsed)
